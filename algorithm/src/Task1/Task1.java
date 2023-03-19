3ad14ca663b540f2772b691a85a559c533fbed30
MykhailoBakanov <117221737+MykhailoBakanov@users.noreply.github.com>
2023-03-19 16:02:12 +0100

    Add files via upload

---
+++ algorithm/src/Task1/Task1.java
@@ -0,0 +1,30 @@
+package Task1;
+
+class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Array of random numbers: ");
+
+        int[] intArray = {3, 5, 10, 22, 7, 9};
+
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+
+        int index = -1;
+
+        int numberToFind = 9;
+        System.out.println("Numbers to search index: " + numberToFind);
+
+        for (int i = 0; i < intArray.length; i++)
+            if (intArray[i] == numberToFind) {
+                index = i;
+                break;
+            }
+
+        System.out.println(index < 0
+                ? "Array do not have element with value: " + numberToFind
+                : "Number '" + numberToFind + "' was found at index '" + index + "'");
+    }
+}
---
+++ algorithm/src/Task2/Task2.java
@@ -0,0 +1,21 @@
+package Task2;
+
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.println("Array of random numbers: ");
+
+        int[] intArray = {1, 2, 3, 4, 5, 6};
+
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+
+        int sumSquared = 0;
+
+        for (int i = 0; i < intArray.length; i++) {
+            sumSquared = sumSquared + intArray[i] * intArray[i];
+        }
+        System.out.println("Sum of squares of all array elements: " + sumSquared);
+    }
+}
